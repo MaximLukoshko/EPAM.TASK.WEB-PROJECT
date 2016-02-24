db7e9a12db85a802c02177b5541f4cd9ef724a80
MaximLukoshko <www.maxim.by97@gmail.com>
2016-02-24 11:43:01 +0300

    Getting connection to database at StartUpServlet

--- src/servlet/StartupServlet.java
+++ src/servlet/StartupServlet.java
@@ -1,9 +1,14 @@
 package servlet;
 
+import java.sql.Connection;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.sql.DataSource;
 
 import entity.Ad;
 import entity.AdList;
@@ -23,24 +28,20 @@ public class StartupServlet extends HttpServlet {
 	 */
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		// Загрузить список пользователей
-		UserList userList = UserListHelper.readUserList(getServletContext());
-		// Сохранить список пользователей в контексте сервлета
-		// (для JSP это тождественно равно applicationContext)
-		getServletContext().setAttribute("users", userList);
-		// Загрузить список сообщений
-		AdList adList = AdListHelper.readAdList(getServletContext());
-		// Сохранить список объявлений в контексте сервлета
-		// (для JSP это тождественно равно applicationContext)
-		getServletContext().setAttribute("ads", adList);
-		for (Ad ad : adList.getAds()) {
-			// Т.к. в сообщениях изначально присутствует только id
-			// автора, для удобства установим ссылки
-			ad.setAuthor(userList.findUser(ad.getAuthorId()));
-			// Инициализировать значения свойства lastModifiedDate
-			ad.setLastModified(ad.getLastModified());
+		Connection conn;
+		InitialContext initContext;
+		DataSource ds;
+		try {
+			initContext = new InitialContext();
+			ds = (DataSource) initContext.lookup("java:comp/env/jdbc/jsp");
+			conn = ds.getConnection();
+			getServletContext().setAttribute("databaseConnection", conn);
+			System.out.println("Server is started");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Connection to Database failed");
 		}
 		System.out.println("Servlet is loaded");
 	}
-	
+
 }
