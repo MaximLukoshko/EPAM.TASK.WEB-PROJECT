2618ab9680c0cb698dcf6a24d9b4b533f4f540b3
MaximLukoshko <www.maxim.by97@gmail.com>
2016-03-05 15:08:34 +0300

    Added Log4j

--- src/servlet/StartupServlet.java
+++ src/servlet/StartupServlet.java
@@ -9,17 +9,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.sql.DataSource;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 /**
  * Servlet implementation class StartupServlet
  */
 @WebServlet("/StartupServlet.do")
 public class StartupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final Logger log = Logger.getLogger(StartupServlet.class);
 
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
+		BasicConfigurator.configure();
 		super.init(config);
 		InitialContext initContext;
 		DataSource ds;
@@ -29,10 +34,12 @@ public class StartupServlet extends HttpServlet {
 			ds = (DataSource) initContext.lookup("java:comp/env/jdbc/epam_test_web");
 			conn = ds.getConnection();
 			getServletContext().setAttribute("databaseConnection", conn);
-			System.out.println("Connection to Database successdully started");
+			log.info("Connection to Database successdully started");
+//			System.out.println("Connection to Database successdully started");
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.out.println("Connection to Database failed");
+			log.error("Connection to Database failed;");
+//			System.out.println("Connection to Database failed");
 		}
 		System.out.println("Servlet is loaded");
 	}
