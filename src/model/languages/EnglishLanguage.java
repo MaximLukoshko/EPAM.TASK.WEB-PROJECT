bcecf756b4b6586b9cc73e71e5fc04c70ba48da2
MaximLukoshko <www.maxim.by97@gmail.com>
2016-03-21 17:16:41 +0300

    Created EnglishLanguage

---
+++ src/model/languages/EnglishLanguage.java
@@ -0,0 +1,35 @@
+package model.languages;
+
+import org.apache.log4j.Logger;
+
+public class EnglishLanguage extends AbstractLanguage {
+	@SuppressWarnings("unused")
+	private static final Logger log = Logger.getLogger(EnglishLanguage.class);
+
+	public EnglishLanguage() {
+		BulletinBoard = "Bulletin Board";
+		BoardName = "Figa List";
+		YouEnteredAs = "You entered as";
+		ListOfAds = "List of Ads";
+		PersonalCabinet = "Personal Cabinet";
+
+		Subject = "Subject";
+		Type = "Type";
+		Author = "Author";
+		LastModifiedDate = "Last-Modified-Date";
+		Login = "Login";
+		Password = "Password";
+		Name = "Name";
+		Email = "E-mail";
+		Body = "Body";
+
+		LoginButtonName = "Log In";
+		SignUpButtonName = "SignUp";
+		CancelButtonName = "Cancel";
+		CreateButtonName = "Create";
+		LogOutButtonName = "Log Out";
+		MainButtonName = "Main";
+		CabinetButtonName = "Cabinet";
+	}
+
+}
