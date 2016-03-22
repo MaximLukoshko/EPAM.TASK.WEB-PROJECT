c6f38c4e7b400647504b8b63bd88b9d6335bcf3b
MaximLukoshko <www.maxim.by97@gmail.com>
2016-03-22 12:59:22 +0300

    Removed unused logging at choosing language . java

--- src/controller/tag/ChooseLanguage.java
+++ src/controller/tag/ChooseLanguage.java
@@ -11,6 +11,7 @@ import org.apache.log4j.Logger;
 import model.languages.LanguageSimpleFactory;
 
 public class ChooseLanguage extends SimpleTagSupport {
+	@SuppressWarnings("unused")
 	private static final Logger log = Logger.getLogger(ChooseLanguage.class);
 
 	private String language;
@@ -20,7 +21,6 @@ public class ChooseLanguage extends SimpleTagSupport {
 		LanguageSimpleFactory languageSimpleFactory = new LanguageSimpleFactory();
 		getJspContext().setAttribute("Language", languageSimpleFactory.getLanguage(language),
 				PageContext.SESSION_SCOPE);
-		log.info("Choosing Language activated");
 	}
 
 	public void setLanguage(String language) {
